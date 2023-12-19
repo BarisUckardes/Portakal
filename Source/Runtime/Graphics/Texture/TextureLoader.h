4f34a7fe8bb310813fa5541a38d2b867a615e2c0
Barış Üçkardeş <68399075+BarisUckardes@users.noreply.github.com>
2023-12-19 17:36:46 +0300

    Stbi texture load utilitiy

---
+++ Source/Runtime/Graphics/Texture/TextureLoader.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <Runtime/Graphics/Texture/Texture.h>
+#include <Runtime/Graphics/Texture/TextureLoadResult.h>
+
+namespace Portakal
+{
+	class RUNTIME_API TextureLoader final
+	{
+	public:
+		static void LoadFromPath(const String& path, TextureLoadResult& resultOut);
+	public:
+		TextureLoader() = delete;
+		~TextureLoader() = delete;
+	};
+}
