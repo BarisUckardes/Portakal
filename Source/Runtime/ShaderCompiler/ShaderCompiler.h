a24e61c43d655e333a903e1182f867b8d9be0fb2
Metehan Tuncbilek <mtuncbilek95@gmail.com>
2023-12-31 18:17:49 +0300

    Shader Compiler

---
+++ Source/Runtime/ShaderCompiler/ShaderCompiler.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Runtime/Containers/String.h>
+#include <Runtime/Memory/MemoryOwnedView.h>
+#include <Runtime/Memory/MemoryView.h>
+#include <Runtime/ShaderCompiler/ShaderCompilerUtils.h>
+#include <Runtime/Graphics/Device/GraphicsBackend.h>
+
+namespace Portakal
+{
+	class RUNTIME_API ShaderCompiler
+	{
+	public:
+		// Compile functions may need GraphicsApi param to determine hlsl to spirv conversions properly.
+		static bool CompileToSPIRV(const String& sourceAsString, const String& entryMethodName, const ShaderStage stage, const ShaderLanguage language, MemoryOwnedView** ppViewOut);
+		static bool CompileToSPIRV(const MemoryOwnedView* pView, const String& entryMethodName, const ShaderStage stage, const ShaderLanguage language, MemoryOwnedView** ppViewOut);
+		static bool CompileFromSPIRV(const MemoryView& pView, GraphicsBackend chosenApi, MemoryOwnedView** ppBytes);
+		static bool CompileFromSPIRV(const MemoryView& pView, const ShaderLanguage language, MemoryOwnedView** ppViewOut);
+	private:
+		ShaderCompiler() = delete;
+		~ShaderCompiler() = delete;
+	};
+}
