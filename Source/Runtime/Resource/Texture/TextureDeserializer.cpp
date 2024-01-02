852d006b5c1c2a4644fad2c531021773b7644165
Barış Üçkardeş <68399075+BarisUckardes@users.noreply.github.com>
2024-01-02 19:51:05 +0300

    TextureResource deserializer half ass implementation

--- Source/Runtime/Resource/Texture/TextureDeserializer.cpp
+++ Source/Runtime/Resource/Texture/TextureDeserializer.cpp
@@ -1,13 +1,28 @@
 #include "TextureDeserializer.h"
+#include <Runtime/Resource/Texture/TextureResource.h>
 
 namespace Portakal
 {
 	ResourceSubObject* TextureDeserializer::Deserialize(const MemoryView& view)
 	{
-		return nullptr;
+		//Create texture
+		TextureResource* pTexture = new TextureResource();
+
+		//pTexture->AllocateTexture();
+
+		//pTexture->UpdateTexture();
+
+		return pTexture;
 	}
 	ResourceSubObject* TextureDeserializer::DeserializeOptimized(const MemoryView& view)
 	{
-		return nullptr;
+		//Create texture
+		TextureResource* pTexture = new TextureResource();
+
+		//pTexture->AllocateTexture();
+
+		//pTexture->UpdateTexture();
+
+		return pTexture;
 	}
 }
