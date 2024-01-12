8d4cf3c7e427d261f0bcee568020a5899103ab76
Metehan Tuncbilek <mtuncbilek95@gmail.com>
2024-01-12 17:07:14 +0300

    Queue Two Variation

--- Source/Runtime/Containers/Queue.h
+++ Source/Runtime/Containers/Queue.h
@@ -10,21 +10,21 @@ namespace Portakal
 	 * @brief It is a FIFO based container.
 	 */
 	template<typename T>
-	class RUNTIME_API Queue
+	class RUNTIME_API QueueDoubleNode
 	{
 	public:
-		Queue() : mBackNode(nullptr), mFrontNode(nullptr), mSize(0)
+		QueueDoubleNode() : mBackNode(nullptr), mFrontNode(nullptr), mSize(0)
 		{
 
 		}
-		Queue(T* data) : mBackNode(nullptr), mFrontNode(nullptr), mSize(0)
+		QueueDoubleNode(T* data) : mBackNode(nullptr), mFrontNode(nullptr), mSize(0)
 		{
 			Push(data);
 		}
-		Queue(const Queue& other) = delete;
-		Queue& operator=(const Queue& other) = delete;
+		QueueDoubleNode(const Queue& other) = delete;
+		QueueDoubleNode& operator=(const QueueDoubleNode& other) = delete;
 
-		~Queue()
+		~QueueDoubleNode()
 		{
 			while (!IsEmpty())
 			{
@@ -94,6 +94,117 @@ namespace Portakal
 	private:
 		Node* mBackNode;
 		Node* mFrontNode;
+		UInt32 mSize;
+	};
+
+	/**
+ * @class Queue
+ *
+ * @brief It is a FIFO based container.
+ */
+	template<typename T>
+	class RUNTIME_API QueueSingleNode
+	{
+	public:
+		QueueSingleNode() : mRootNode(nullptr), mSize(0)
+		{
+
+		}
+		QueueSingleNode(T* data) : mRootNode(nullptr), mSize(0)
+		{
+			Push(data);
+		}
+		QueueSingleNode(const QueueSingleNode& other) = delete;
+		QueueSingleNode& operator=(const QueueSingleNode& other) = delete;
+
+		~QueueSingleNode()
+		{
+			while (!IsEmpty())
+			{
+				Pop();
+			}
+		}
+
+		/**
+		 * @brief Adds an element to the end of the queue.
+		 */
+		void Push(T* data)
+		{
+			// Check if queue is empty.
+			if (mRootNode == nullptr)
+			{
+				mRootNode = new Node();
+				mRootNode->mData = data;
+				mRootNode->mNext = nullptr;
+			}
+			else
+			{
+				// Create a new node and add it to the beginning.
+				Node* newNode = new Node();
+				newNode->mData = data;
+				newNode->mNext = mRootNode;
+				mRootNode = newNode;
+			}
+
+			mSize++;
+		}
+
+		/**
+		 * @brief Removes an element from the beginning of the queue.
+		 *
+		 */
+		void Pop()
+		{
+			// Check if queue is empty.
+			if (mRootNode == nullptr)
+			{
+				DEV_LOG("Queue", "There is nothing to pop!");
+				return;
+			}
+
+			// Check if queue has only one element.
+			if (mRootNode->mNext == nullptr)
+			{
+				delete mRootNode;
+				mRootNode = nullptr;
+
+				mSize--;
+				return;
+			}
+
+			// Check if queue has more than one element.
+			if (mRootNode->mNext != nullptr)
+			{
+				// Find the next node;
+				Node* currentNode = mRootNode->mNext;
+				// Delete the RootNode, so the next node will be the new root node.
+				delete mRootNode;
+				// Set the new root node.
+				mRootNode = currentNode;
+
+				mSize--;
+			}
+		}
+
+		void Clear()
+		{
+			while (!IsEmpty())
+			{
+				Pop();
+			}
+		}
+
+		UInt32 Size() const { return mSize; }
+		bool IsEmpty() const { return mSize == 0; }
+
+	public:
+		struct Node
+		{
+			T* mData;
+			Node* mNext;
+		};
+	private:
+		Node* mRootNode;
 		UInt32 mSize;
 	};
 }
