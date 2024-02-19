514c50e2db8ffa2c32c43e6bbd40d933a0e7a116
Barış Üçkardeş <68399075+BarisUckardes@users.noreply.github.com>
2024-02-19 19:07:11 +0300

    Initial Start

---
+++ Scripts/CMake/FetchLibraries.cmake
@@ -0,0 +1,48 @@
+cmake_minimum_required(VERSION 3.26)
+
+include(FetchContent)
+
+# STB_IMAGE library
+FetchContent_Declare(
+    stbi
+    GIT_REPOSITORY https://github.com/nothings/stb.git
+)
+
+# ASSIMP library
+FetchContent_Declare(
+    assimp
+    GIT_REPOSITORY https://github.com/assimp/assimp.git
+)
+
+# IMGUI library
+FetchContent_Declare(
+    imgui
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_TAG docking
+)
+
+# IMGUIZMO library
+FetchContent_Declare(
+	imguizmo
+	GIT_REPOSITORY https://github.com/CedricGuillemet/ImGuizmo.git
+    GIT_TAG "1.83"
+)
+
+# YAML-CPP library
+FetchContent_Declare(
+	yaml-cpp
+	GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
+)
+
+# SPIRV reflect
+FetchContent_Declare(
+    spirv-reflect
+    GIT_REPOSITORY https://github.com/KhronosGroup/SPIRV-Reflect.git
+    GIT_TAG main
+)
+
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build shared libraries" FORCE)
+set(ASSIMP_BUILD_TESTS OFF CACHE BOOL "Assimp build tests" FORCE)
+set(ASSIMP_INSTALL OFF CACHE BOOL "Assimp install" FORCE)
+set(ASSIMP_INSTALL_PDB OFF CACHE BOOL "Assimp install PDB" FORCE)
+set(SPIRV_REFLECT_STATIC_LIB TRUE)
